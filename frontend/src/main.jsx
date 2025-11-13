2031fdebad8f480ed98caa0865755c3de061f739
Giridharan <pgiridharan2002@gmail.com>
2025-11-13 17:44:50 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import authReducer from './store/authSlice';
+
+// Configure the Redux store
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render the application, wrapped in providers for state and routing
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
